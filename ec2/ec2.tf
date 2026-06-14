a0201b8da3f0a4d3a49688123b6f6946b5c4db52
prasanth <durgaprasanth003@gmail.com>
2026-06-14 13:45:21 +0530

    hello world

---
+++ ec2/ec2.tf
@@ -0,0 +1,33 @@
+resource "aws_instance" "roboshop" {
+  ami           = ami-0220d79f3f480ecf5
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_security_group" "allow all" {
+  name        = "allow all"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  tags = {
+    Name = "allow all"
+  }
+
+    ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+    egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+}
