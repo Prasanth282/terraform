bf4d47b0ded32ecaf6a80d7639e49f59246e0db0
prasanth <durgaprasanth003@gmail.com>
2026-06-14 18:30:38 +0530

    hello world

---
+++ conditions/ec2.tf
@@ -0,0 +1,30 @@
+resource "aws_instance" "roboshop" {
+  ami                    = var.ami_id
+  instance_type          = var.environment == "dev" ? "t.3 micro" : "t.2 small"
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+
+  tags = var.ec2_tags
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = var.sg_name
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  tags = var.sg_tags
+
+  ingress {
+    from_port        = var.from_port
+    to_port          = var.to_port
+    protocol         = "-1"
+    cidr_blocks      = var.cidr_blocks
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  egress {
+    from_port        = var.from_port
+    to_port          = var.to_port
+    protocol         = "-1"
+    cidr_blocks      = var.cidr_blocks
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+}
