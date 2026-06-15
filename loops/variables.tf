d85e44ab9825eaba4b1a5cd881e59c020668b3a2
prasanth <durgaprasanth003@gmail.com>
2026-06-15 12:45:04 +0530

    hello world

---
+++ loops/variables.tf
@@ -0,0 +1,54 @@
+variable "ami_id" {
+  default     = "ami-0220d79f3f480ecf5"
+  type        = string
+  description = "ami id of instance"
+}
+
+variable "instance_type" {
+  default = "t.3 micro"
+}
+
+variable "ec2_tags" {
+  type = map(string)
+  default = {
+    Name    = "roboshop"
+    purpose = "ec2 instance creation"
+
+  }
+}
+
+variable "sg_name" {
+  default = "allow_all"
+}
+
+variable "sg_tags" {
+  default = {
+    Name = "allow_all"
+  }
+}
+
+variable "from_port" {
+  type    = number
+  default = 0
+}
+variable "to_port" {
+  type    = number
+  default = 0
+}
+
+variable "cidr_blocks" {
+  type = list(string)
+
+  default = ["0.0.0.0/0"]
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+
+
+
+
+
+
